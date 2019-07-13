851d2bca5e3fd6c3486638988985b52e90dc8c54
Minseung Kwon <bananapizzza@gmail.com>
2019-07-12 17:34:55 -0700

    Changed SelectWorkout from a function to a class to save a state.

--- src/components/SelectWorkout.js
+++ src/components/SelectWorkout.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SelectWorkout = ({onClick, selectedWorkout}) => (
-    <div>
-        <button onClick={onClick}>
-            {selectedWorkout ? 'Select Again' : 'Select Workout Randomly'}
-        </button>
-        <p>
-            {selectedWorkout}
-        </p>
-    </div>
-);
+class SelectWorkout extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {workoutList: ""};
+    }
+    getWorkoutList = () => {
+        fetch("http://localhost:5000/").then(result => {
+            if (result.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return result.json();
+        }).then((data) => {
+            console.log(data);
+            this.setState({
+                workoutList: data
+            });
+        });
+    };
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.props.onClick}>
+                    {this.props.selectedWorkout ? 'Select Again' : 'Select Workout Randomly'}
+                </button>
+                <p>
+                    {this.props.selectedWorkout}
+                </p>
+                <button onClick={this.getWorkoutList}>
+                    Get Workout List
+                </button>
+                <p>
+                    {this.state.workoutList}
+                </p>
+            </div>
+        );
+    }
+}
+
+
 
 SelectWorkout.propTypes = {
     onClick: PropTypes.func.isRequired,
     selectedWorkout: PropTypes.string.isRequired
-}
+};
 
 
 export default SelectWorkout;
