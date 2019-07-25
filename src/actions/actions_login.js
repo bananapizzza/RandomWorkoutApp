84045c7a61bf20db5708a43500364fcf69fa33f1
Minseung Kwon <bananapizzza@gmail.com>
2019-07-25 16:34:24 -0700

    Changed to use async/await.

--- src/actions/actions_login.js
+++ src/actions/actions_login.js
@@ -1,45 +1,45 @@
 import fetch from "cross-fetch";
 
+//Action list
 export const REQUEST_LOGIN = 'REQUEST_LOGIN';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 
+//Action for requesting login
 export const requestLogin = () => ({
     type: REQUEST_LOGIN
 });
 
+//Action for login success
 export const loginSuccess = (userInfo) => ({
     type: LOGIN_SUCCESS,
     username: userInfo
 });
 
+//Action for login fail
 export const loginFail = (error) => ({
     type: LOGIN_FAIL,
     loginError: error
 });
 
-export const loginInfoSubmit = (values, dispatch) => {
+//Action for clicking Log in button
+export const loginInfoSubmit = async (values, dispatch) => {
     dispatch(requestLogin());
-    let isLoginFail = false;
-    fetch("http://localhost:5000/check_login_info", {
+
+    const response = await fetch("http://localhost:5000/check_login_info", {
         method: "POST",
         headers: {
             'Content-type': 'application/json',
         },
         body: JSON.stringify(values)
-    }).then(result => {
-        if (result.status >= 400) {
-            isLoginFail = true;
-        }
-        return result.json();
-    }).then((json) => {
-            if(isLoginFail){
-                dispatch(loginFail(json));
-            } else {
-                dispatch(loginSuccess(values.username));
-            }
-        }
-    );
+    });
+
+    const result = await response.json();
+    if(response.status >= 400) {
+        dispatch(loginFail(result));
+    } else {
+        dispatch(loginSuccess(values.username));
+    }
 };
 
 export default loginInfoSubmit;
